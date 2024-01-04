e25818b35a952bb788f5908e4b9595091a840da0
hase <patrickahase@gmail.com>
2024-01-04 16:11:22 +1100

    updated express server - static folder path

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ const app = express();
 //app.use(cors());
 
 // serve static files from ./public
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'placeholder')));
 
 // serve placeholder index at root url
 app.get('/', (req, res) => {
   res.sendFile(__dirname, '/placeholder/index.html');
 });
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 33063;
 app.listen(port, () => console.log(`Server started on port ${port}`));
