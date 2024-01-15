8250a618906201952ab2f4b8c1b5d951b9da1545
hase <patrickahase@gmail.com>
2024-01-15 11:18:46 +1100

    added test webhook reception

--- server.js
+++ server.js
@@ -19,5 +19,20 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname, '/placeholder/index.html');
 });
 
+// listen for webhook on repo main branch update
+app.post('/webhook/update', express.json({type: 'application/json'}), (req, res) => {
+  // confirm webhook accepted
+  res.status(202).send('Accepted');
+
+  // check what kind of event was triggered
+  const githubEvent = req.headers['x-github-event'];
+
+  if (githubEvent === 'push'){
+    console.log("push")
+  } else {
+    console.log(`Unhandled event: ${githubEvent}`);
+  }
+});
+
 const port = process.env.PORT || 33063;
 app.listen(port, () => console.log(`Server started on port ${port}`));
