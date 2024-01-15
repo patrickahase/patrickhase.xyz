55ecd17a28338ea887130de326d757bf96400d04
hase <patrickahase@gmail.com>
2024-01-15 11:38:02 +1100

    trigger script on push

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
+// allow server execution
+var exec = require('child_process').exec;
 //const cors = require('cors'); // cors not installed yet
 
 const app = express();
@@ -28,8 +30,9 @@ app.post('/webhook/update', express.json({type: 'application/json'}), (req, res)
   const githubEvent = req.headers['x-github-event'];
 
   if (githubEvent === 'push'){
-    // if something pushed to main
-    console.log("push")
+    // if something pushed to main run sh script
+    console.log("push");
+    exec("./bashscripts/pull-from-repo.sh");
   } else {
     // this logs any unhandled event
     console.log(`Unhandled event: ${githubEvent}`);
